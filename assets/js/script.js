2fca6d0abb171f98b5e3729a7a3ddc1d5e8061cc
Steven-McCombe <smccombe93@gmail.com>
2022-11-07 23:42:26 -0500

    created functions to render weather,departures and arrivals.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Global Variables ----------
 var dayNow = moment().format('dddd')
 var dateNow = moment().format('L')
-
+var numberOfFlights = 5
 // Render Data and Time to Weather Card.
 $("#dateWeather").html(dateNow)
 $("#WeatherTitleEl").html(dayNow)
@@ -96,7 +96,133 @@ var requestWeather = {
   redirect: 'follow',
 };
 
-fetch(WEATHER_API_URL, requestWeather)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// function to render flight cards
+function renderFlightInfo() {
+  for (i = 0; i <= numberOfFlights; i++) {
+      //Render Departure Flights.
+      $('#departureContainer').append(`<div class="card flightCardCustom" style="width: 100%;">
+          <div class="row g-0">
+            <div class="col-md-9 flightInfoCustom ">
+              <div class="card-body">
+                    <h6 class="card-title flightText">Destination: <span id="departureTitle${i}">[Destination Airport Name]</span></h6>
+                    <h6 class="card-title flightText">Flight: <span id="departureFlightName${i}">[Airline - Flight No]</span></h6>
+                    <h6 class="card-title flightText">Sch Time/Date: <span id="departureSchTime${i}">[time/date]</span></h6>
+                    <h6 class="card-title flightText">Destination Arrival: <span id="departureArrTime${i}">[Time]</span></h6>
+                    <h6 class="card-title flightText">Terminal: <span id="departureTerminal${i}">[Terminal]</span></h6>
+                    <h6 class="card-title flightText">Gate: <span id="departureGate${i}">[Gate #]</span></h6>
+                    <h6 class="card-title flightText">Flight Duration: <span id="departureDuration${i}"></span></h6>
+              </div>
+            </div>
+            <div class="col-md-3 d-flex align-items-center justify-content-center">
+                    <h6 id="departureStatus${i}">[On Time]</h6>
+            </div>
+          </div>`)  
+      //Render Arriving Flights.
+      $('#arrivalContainer').append(`<div class="card flightCardCustom" style="width: 100%;">
+          <div class="row g-0 flightCard">
+            <div class="col-md-9 flightInfoCustom">
+                <div class="card-body">
+                    <h6 class="card-title flightText">Departed From: <span id="arrivalTitle${i}">[Departed From Airport Name]</span></h6>
+                    <h6 class="card-title flightText">Flight: <span id="arrivalFlightName${i}">[Airline - Flight No]</span></h6>
+                    <h6 class="card-title flightText">Departed Time: <span id="arrivalDepartedTime${i}">[time/date]</span></h6>
+                    <h6 class="card-title flightText">Sch.Arrival Time: <span id="arrivalSchTime${i}">[estTime]</span></h6>
+                    <h6 class="card-title flightText">Terminal: <span id="arrivalTerminal${i}">[Terminal]</span></h6>
+                    <h6 class="card-title flightText">Gate: <span id="arrivalGate${i}">[Gate #]</span></h6>
+                    <h6 class="card-title flightText">Flight Status: <span id="arrivalFlightStatus${i}">[Flight Status]</span></h6>
+                </div>
+            </div>
+            <div class="col-md-3 d-flex align-items-center justify-content-center">
+                <h6 id ="arrivalDelayStatus${i}">[On-Time]</h6>
+            </div>
+          </div>`)
+  }
+}
+
+// function to render departures
+function getDepartures() {
+  fetch(AIRLABS_SCHEDULES_DEP_API_URL)
+    .then(function (response) {
+      if (!response.ok) {
+        console.error("")
+        throw response.json();
+      }
+      return response.json();
+    })
+    
+    .then(function (returnResults) {
+      console.log(returnResults)
+
+      for (var i = 0; i <= numberOfFlights; i++) {
+          $("#departureTitle" + (i)).html(returnResults.response[i].arr_iata)
+          $("#departureFlightName" + (i)).html(returnResults.response[i].flight_iata)
+          $("#departureSchTime" + (i)).html(returnResults.response[i].dep_time)
+          $("#departureArrTime" + (i)).html(returnResults.response[i].arr_time)
+          $("#departureTerminal" + (i)).html(returnResults.response[i].dep_terminal)
+          $("#departureGate" + (i)).html(returnResults.response[i].dep_gate)
+          $("#departureDuration" + (i)).html(returnResults.response[i].duration + " Minutes")
+        //this logic is not working
+          if (returnResults.response[i].delayed === null) {
+            $("#departureStatus"+(i)).html("On Time").attr("class","ontime")
+          } else {
+            $("#departureStatus"+(i)).html("Delayed by: " + returnResults.response[i].delayed + " Mins").attr("class", "delayed")
+          }
+        } 
+    })
+}
+// function to render arrivals
+function getArrivals() {
+  fetch(AIRLABS_SCHEDULES_ARR_API_URL)
+    .then(function (response) {
+      if (!response.ok) {
+        console.error("")
+        throw response.json();
+      }
+      return response.json();
+    })
+  
+    .then(function (returnResults) {
+      console.log(returnResults)
+      for (var i = 0; i <= numberOfFlights; i++) {
+        $("#arrivalTitle" + (i)).html(returnResults.response[i].dep_iata)
+        $("#arrivalFlightName" + (i)).html(returnResults.response[i].flight_iata)
+        $("#arrivalDepartedTime" + (i)).html(returnResults.response[i].dep_time)
+        $("#arrivalSchTime" + (i)).html(returnResults.response[i].arr_time)
+        $("#arrivalTerminal" + (i)).html(returnResults.response[i].arr_terminal)
+        $("#arrivalGate" + (i)).html(returnResults.response[i].arr_gate)
+        $("#arrivalFlightStatus" + (i)).html(returnResults.response[i].status)
+        if (returnResults.response[i].arr_delayed === null) {
+          $("#arrivalDelayStatus" + (i)).html("On Time").attr("class", "ontime")
+        } else {
+          $("#arrivalDelayStatus" + (i)).html("Delayed by: " + returnResults.response[i].dep_delayed + " Mins").attr("class", "delayed")
+        }
+      }
+    })
+}
+//Function to render the weather data to the page. 
+function getWeather() {
+  fetch(WEATHER_API_URL)
+    .then(function (response) {
+      if (!response.ok) {
+        console.error("")
+        throw response.json();
+      }
+      return response.json();
+      
+    })
+    .then(function (returnResults) {
+      $("#imgWeather").attr("src", "https:" + returnResults.current.condition.icon)
+      $("#dateWeather").html(returnResults.location.localtime)
+      $("#tempWeather").html(returnResults.current.temp_f.toFixed() + "°F")
+      $("#humidityWeather").html(returnResults.current.humidity.toFixed() + "%")
+      $("#windWeather").html(returnResults.current.wind_mph.toFixed() + "mph")
+      $("#conditionsWeather").html(returnResults.current.condition.text)
+    })
+}
+
+
+renderFlightInfo()
+getWeather()
+getDepartures()
+getArrivals()
+console.log(AIRLABS_SCHEDULES_ARR_API_URL)
+
